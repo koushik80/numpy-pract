b5b4404c9f6c9eae185ee7ee65125360f7b16f4d
Koushik Dey <koushik.dey@live.com>
2022-11-11 17:28:29 +0200

    numpy array shapes

--- test.py
+++ test.py
@@ -9,7 +9,43 @@ import numpy as np
 
 # multi-dimensional array
 
-myList = [[1, 2, 3, 4, 5], [7, 8, 9, 10, 11], [12, 13, 14, 15, 16]]
+#myList = [[1, 2, 3, 4, 5], [7, 8, 9, 10, 11], [12, 13, 14, 15, 16]]
 
-n = np.array(myList)
-print(n)
+#n = np.array(myList)
+#print(n)
+#print(n.shape)
+
+
+#n = np.arange(0,7)
+#print(n);
+
+
+#zero = np.zeros(100)
+#print(zero);
+
+#one = np.ones(100)
+#print(one);
+
+#a = np.eye(10)
+#print(a);
+
+#mn = np.arange(0,50)
+#mn
+#print(len(mn))
+
+#arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+#newarr = arr.reshape(4, 3)
+
+#print(newarr)
+
+
+#arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+#newarr = arr.reshape(2, 2, -1)
+
+#print(newarr)
+
+
+#o = np.linspace(0,7,10)
+#print(o)
