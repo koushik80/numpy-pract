2a4cdfac9087dc41abe2627aee83e437605b2587
Koushik Dey <koushik.dey@live.com>
2022-11-11 16:14:00 +0200

    numpy array

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+# single array
+#myList = [1, 2, 3, 4, 5]
+#n = np.array(myList)
+
+#print(n)
+
+# multi-dimensional array
+
+myList = [[1, 2, 3, 4, 5], [7, 8, 9, 10, 11], [12, 13, 14, 15, 16]]
+
+n = np.array(myList)
+print(n)
