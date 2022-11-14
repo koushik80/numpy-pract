273ffd0f197f28d9f483474a932b05a92a828f8f
Koushik Dey <koushik.dey@live.com>
2022-11-14 18:25:09 +0200

    loc & iloc

--- pandas/test.py
+++ pandas/test.py
@@ -12,25 +12,43 @@ import pandas as pd
 
 import pandas as pd
 
+# -----------------Serries---------------
 #animals = ['Cat', 'Dog', 'Bird', 'Rabit', 'Duck', 'Cow', 'Tiger', 'Lion']
 
 #animal_list = pd.Series(animals)
 
 # print(animal_list)
 
-courses = pd.Series(['React', 'Node', 'Python', 'NumPy', 'Pandas'], index=[
-                    'Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
-print(courses)
+# ------------------
 
-days = pd.Series(['Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
-print(days)
+# --------------------
+# courses = pd.Series(['React', 'Node', 'Python', 'NumPy', 'Pandas'], index=[
+#    'Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
+# print(courses)
 
-week_day = pd.Series(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
-print(week_day)
+#days = pd.Series(['Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
+# print(days)
 
-course_schedule = pd.Series(['React', 'Node', 'Python', 'NumPy', 'Pandas'], index=[
-                            'Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
-print(course_schedule)
+#week_day = pd.Series(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
+# print(week_day)
 
-complete_schedule = courses + ' ' + week_day
-print(complete_schedule)
+# course_schedule = pd.Series(['React', 'Node', 'Python', 'NumPy', 'Pandas'], index=[
+#    'Day1', 'Day2', 'Day3', 'Day4', 'Day5'])
+# print(course_schedule)
+
+#complete_schedule = courses + ' ' + week_day
+# print(complete_schedule)
+# ----------------
+
+
+# --------loc & iloc--------
+
+sports = {'Football': 'Spain',
+          'NBA': 'USA',
+          'Cricket': 'India',
+          'Athelets': 'Jamaica'}
+
+sports_series = pd.Series(sports)
+print(sports_series)
+print(sports_series.loc['Cricket'])
+print(sports_series.iloc[3])
